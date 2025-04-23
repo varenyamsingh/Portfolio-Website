474d2ff4782dd089239cbf969cabb4b01685100c
varenyamsingh <varenyamsingh1330@gmail.com>
2025-04-23 18:14:54 +0530

    New Commit

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Frontend Developer", "MERN Stack Developer"],
+        strings: ["Data Analyst", "Frontend Developer", "MERN Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Frontend Developer", "MERN Stack Developer"],
+        strings: ["Data Analyst", "Frontend Developer", "MERN Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
